6948651646647cc1a0c41638a56649af62d21f2b
CalledCracki <58007536+CalledCracki@users.noreply.github.com>
2023-06-04 18:50:24 +0200

    Commit project

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    void Awake() {
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume; 
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(String name) {
+       Sound s = Array.Find(sounds, sound => sound.name == name);
+       s.source.Play();
+    }
+}
